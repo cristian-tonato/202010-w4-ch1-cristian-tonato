e1857245364ecb7ccf120e7e08b2abca438287c4
Cristian <cristianotona@gmail.com>
2022-10-24 18:53:01 +0200

    fail

---
+++ src/components/sample.tsx
@@ -0,0 +1,15 @@
+//import { Counter } from "./counter";
+//import { CounterComplex } from "./counter.complex";
+//import style from "./sample.module.css";
+//import `./sample.css`
+
+export function Sample() {
+  const title: string = "hola mundo";
+  return (
+    <>
+      <div>
+        <h1>{title.toUpperCase()} </h1>
+      </div>
+    </>
+  );
+}
